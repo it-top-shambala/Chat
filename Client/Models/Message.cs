da80e5dbd7fb480f200f931225b9b2726e890eb0
Westilier <vlad2007gvv@gmail.com>
2026-01-26 20:21:31 +0300

    Выполнено

---
+++ Client/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Models;
+
+public class Message
+{
+    public int Id { get; set; }
+    public string Username { get; set; } =string.Empty;
+    public string Text { get; set; }
+}
