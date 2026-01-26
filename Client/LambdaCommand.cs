7a7b91a4b2eb79ab8a8dd413bad949ac3a083de1
Darky <roma.artur.alekseev@mail.ru>
2026-01-26 20:27:21 +0300

    правка

--- Client/LambdaCommand.cs
+++ Client/LambdaCommand.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace Client;
-public class RelayCommand : ICommand
+public class LambdaCommand : ICommand
 {
     private readonly Action<object> _execute;
     private readonly Func<object, bool> _canExecute;
 
-    public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+    public LambdaCommand(Action<object> execute, Func<object, bool> canExecute = null)
     {
         _execute = execute ?? throw new ArgumentNullException(nameof(execute));
         _canExecute = canExecute;
