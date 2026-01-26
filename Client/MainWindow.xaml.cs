86a25783260b399d537b0411a89ba1ff7661d38c
AndreyKiyatkin <kiyatkinnikolay29101982@gmail.com>
2026-01-26 21:49:55 +0400

    вваи

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -8,17 +8,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Client.ViewModels;
 
 namespace Client
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
     }
 }
